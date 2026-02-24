2e290472725df3dc6a4933a9a65211060c57e99c
angular-teacher <aditeacherladaat@gmail.com>
2026-02-24 22:30:12 +0200

    feat: add basic content to `input` component - input tag with 2-way binding

--- src/app/input/input.ts
+++ src/app/input/input.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.html',
   styleUrl: './input.scss',
 })
 export class Input {
-
+    text = '';
 }
